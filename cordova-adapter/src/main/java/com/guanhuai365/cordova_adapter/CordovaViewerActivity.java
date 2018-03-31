077beb5079f85a9f42181cd1f4bb0ac7a06705b1
mac <mac@guanhuai365.com>
2018-03-31 23:46:44 +0800

    a

---
+++ cordova-adapter/src/main/java/com/guanhuai365/cordova_adapter/CordovaViewerActivity.java
@@ -0,0 +1,13 @@
+package com.guanhuai365.cordova_adapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CordovaViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cordova_viewer);
+    }
+}
