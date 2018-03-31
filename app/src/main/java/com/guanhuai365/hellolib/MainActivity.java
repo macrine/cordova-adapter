0f9d6bde525fe2aba663a9d21c47deb1dd0b2c1f
mac <mac@guanhuai365.com>
2018-03-31 23:30:01 +0800

    1

---
+++ app/src/main/java/com/guanhuai365/hellolib/MainActivity.java
@@ -0,0 +1,13 @@
+package com.guanhuai365.hellolib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
